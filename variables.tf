7e63f45366420877731cf34b4639f5f5fb7309b8
nfrappart <61788760+nfrappart@users.noreply.github.com>
2021-01-15 17:07:12 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,49 @@
+#############################################
+# This module allows the creation of a VNet #
+#############################################
+
+#Variable declaration for Module
+
+variable "VNetPeeringName" {
+  type    = string
+
+}
+
+variable "RgName" {
+  type    = string
+
+}
+
+variable "LocalVNetName" {
+  type    = string
+
+}
+
+variable "RemoteVNetId" {
+  type    = string
+
+}
+
+variable "IsVirtualNetworkAcccessAllowed" {
+  type    = string
+  default = "false"
+
+}
+
+variable "IsForwardedTrafficAllowed" {
+  type    = string
+  default = "false"
+
+}
+
+variable "IsGWTransitAllowed" {
+  type    = string
+  default = "false"
+
+}
+
+variable "UseRemoteGW" {
+  type    = string
+  default = "false"
+
+}
